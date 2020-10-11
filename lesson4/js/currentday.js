2c5e22676fc6fea1b657bb004605135de253c1fd
lichinchen <che20014@byui.edu>
2020-10-11 00:11:20 -0400

    update

--- lesson4/js/currentday.js
+++ lesson4/js/currentday.js
@@ -2,8 +2,8 @@
 const menubutton = document.querySelector('.responsive-menu');
 const mainnav = document.querySelector('.navigation')
 
-menubutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+menubutton.addEventListener('click', () => {mainnav.classList.toggle('responsive-menu')}, false);
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive-menu')};
 
 
 const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
