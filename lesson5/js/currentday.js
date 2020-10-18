07cb909a7c016dfa13e549caca17fc48f1e387b0
lichinchen <che20014@byui.edu>
2020-10-18 01:55:31 -0400

    change

--- lesson5/js/currentday.js
+++ lesson5/js/currentday.js
@@ -1,10 +1,15 @@
-
-const menubutton = document.querySelector('.responsive-menu');
+const hambutton = document.querySelector('.responsive-menu');
 const mainnav = document.querySelector('.navigation')
 
-menubutton.addEventListener('click', () => {mainnav.classList.toggle('responsive-menu')}, false);
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive-menu')};
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+
+document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 
 const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
 document.getElementById('currentday').textContent = new Date().toLocaleDateString('en-US', options);
+
+
+
+
