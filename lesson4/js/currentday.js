81a031869f54fb539eccd76327e54ee4f863ad26
lichinchen <che20014@byui.edu>
2020-10-24 18:31:07 -0400

    revised

--- lesson4/js/currentday.js
+++ lesson4/js/currentday.js
@@ -1,10 +1,12 @@
 
-const menubutton = document.querySelector('.responsive-menu');
+
+// Hamburger Button
+const hambutton = document.querySelector('.menu');
 const mainnav = document.querySelector('.navigation')
 
-menubutton.addEventListener('click', () => {mainnav.classList.toggle('responsive-menu')}, false);
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive-menu')};
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
-
+//Current date
 const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
 document.getElementById('currentday').textContent = new Date().toLocaleDateString('en-US', options);
