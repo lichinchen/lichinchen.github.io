c4ab1c720015601febcdb95014d33f840ad5c26e
lichinchen <che20014@byui.edu>
2020-10-24 18:12:07 -0400

    change

--- lesson5/js/top.js
+++
@@ -1,5 +0,0 @@
-let date = new Date();
-date.getDay()
-if (date.getDay()==6){
-    document.querySelector(".top-text").style.display="block" 
-}
---
+++ lesson6/js/currentday.js
@@ -0,0 +1,13 @@
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+
+const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
+document.getElementById('currentday').textContent = new Date().toLocaleDateString('en-US', options);
+
+
