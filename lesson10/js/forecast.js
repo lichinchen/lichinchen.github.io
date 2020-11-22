f02c567cf582fc212a24f240a3dbc7fb42fd1090
lichinchen <che20014@byui.edu>
2020-11-22 09:37:54 -0500

    change

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -12,7 +12,7 @@
             var today = parseInt(new Date(days[0].dt_txt).getDate());
             var x = 0;
            
-            //check if the time is 6pm
+            
             for (let i = 0; i < days.length; i++) {
                 if (days[i].dt_txt == `2020-11-${today} 18:00:00`) {
                     newDays[x] = days[i];
@@ -20,18 +20,18 @@
                     today++;
                 }
             }
-            //temperature sometimes i get an error here
+            
             let forecastTemp = document.getElementsByClassName('forTemp');
             for (let i = 0; i < forecastTemp.length; i++) {
                 forecastTemp[i].innerHTML = newDays[i].main.temp;
             }
-            //icon
+            
             let weatherIcon = document.getElementsByClassName("forimg");
             for (let i = 0; i < weatherIcon.length; i++) {
                 weatherIcon[i].setAttribute("src", `https://openweathermap.org/img/wn/${newDays[i].weather[0].icon}@2x.png`);
                 weatherIcon[i].setAttribute("alt", `Icon representing ${newDays[i].weather[0].description}`);
             }
-            //days of the week
+            
             let weatherDay = document.getElementsByClassName('day');
             for (let i = 0; i < weatherDay.length; i++) {
                 let longDate = new Date(newDays[i].dt_txt);
