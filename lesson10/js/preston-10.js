dc50c2e2fa95e413f348ddf6a7bc876477ba1f68
lichinchen <che20014@byui.edu>
2020-11-22 04:11:39 -0500

    change

--- lesson10/js/preston-10.js
+++ lesson10/js/preston-10.js
@@ -1,6 +1,6 @@
 
-const forecast = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=5539d42cf2d71dde5c4f1a28929669c0&units=imperial';
-fetch(forecast)
+const forecastURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=5539d42cf2d71dde5c4f1a28929669c0&units=imperial';
+fetch(forecastURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
