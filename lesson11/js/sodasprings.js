553708030ef980da696de040d1237f8eef6bc58f
lichinchen <che20014@byui.edu>
2020-11-26 19:47:19 -0500

    change

--- lesson11/js/sodasprings.js
+++ lesson11/js/sodasprings.js
@@ -47,41 +47,29 @@ document.getElementById("windChill").textContent = windChill(t,s);
 
 
 
-const forecast= 'https://api.openweathermap.org/data/2.5/forecast?id=5607916&appid=5539d42cf2d71dde5c4f1a28929669c0&units=imperial';
-  
-fetch(forecast)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObject) {
-        const days = jsonObject['list'];
-        console.log(days);
-        var newDays = [];
-        var today = parseInt(new Date(days[0].dt_txt).getDate());
-        var x = 0;
-           
-            
-        for (let i = 0; i < days.length; i++) {
-            if (days[i].dt_txt == `2020-11-${today} 18:00:00`) {
-                newDays[x] = days[i];
-                x++;
-                today++;
-            }
-        }
-        let forecastTemp = document.getElementsByClassName('forTemp');
-        for (let i = 0; i < forecastTemp.length; i++) {
-            forecastTemp[i].innerHTML = newDays[i].main.temp;
-        }
-        let weatherIcon = document.getElementsByClassName("forimg");
-        for (let i = 0; i < weatherIcon.length; i++) {
-            weatherIcon[i].setAttribute("src", `https://openweathermap.org/img/wn/${newDays[i].weather[0].icon}@2x.png`);
-            weatherIcon[i].setAttribute("alt", `Icon representing ${newDays[i].weather[0].description}`);
-        }
-        let weatherDay = document.getElementsByClassName('days');
-        for (let i = 0; i < weatherDay.length; i++) {
-            let longDate = new Date(newDays[i].dt_txt);
-            weatherDay[i].textContent = longDate.toLocaleString("en-us",{weekday:"long"});
-        }
+const forecast = "https://api.openweathermap.org/data/2.5/forecast?id=5607916&appid=5539d42cf2d71dde5c4f1a28929669c0&units=imperial";
+
+    fetch(forecast)
+    .then((response) => response.json())
+    .then((jsObject) => {
+      console.log(jsObject);
+
+        const forecast = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+        console.log(forecast);
+        let day = 0;
+        const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+         
+        forecast.forEach(x => {
+           const d = new Date(x.dt_txt);
+           const desc = x.weather[0].description;
+           const image = "https://openweathermap.org/img/w/" + x.weather[0].icon + ".png";
+          document.getElementById(`dayofweek${day+1}`).textContent = week[d.getDay()];
+          document.getElementById(`forecast${day+1}`).textContent = x.main.temp.toFixed(0) + `°F`;
+          document.getElementById(`icon${day+ 1}`).setAttribute('src', image);
+          document.getElementById(`icon${day+ 1}`).setAttribute('alt', desc);
+          day++;
+           }) 
     });
+
 
 
