8fa701b8aa0791ccf95e78d29451d12c633c7612
lichinchen <che20014@byui.edu>
2020-11-26 20:56:48 -0500

    change

--- lesson10/js/preston-10.js
+++ lesson10/js/preston-10.js
@@ -15,3 +15,33 @@ fetch(apiURL)
     
 });
       
+window.onload = getWindChill();
+function getWindChill() {
+    let t = parseFloat( document.getElementById('curTemp').textContent);
+    let s = parseFloat(document.getElementById('windSpeed').textContent);
+    let windChill = document.getElementById("windChill").innerHTML = "";
+
+    if ((t <= 50) && (s > 3.0)) {
+        windChill = (35.74 + (0.6215 * t) - (35.75 * (Math.pow(s, 0.16))) + (0.4275 * t * (Math.pow(s, 0.16)))).toFixed(0);
+       let windChill = document.getElementById('windChill').innerHTML = windChill;
+    } 
+    else {
+        let windChill = document.getElementById("windChill").innerHTML = "N/A" ;
+    }
+}
+
+var t= document.getElementById("curTemp").textContent;
+var s= document.getElementById("windSpeed").textContent;
+function windChill(t,s) {
+  if((t<=60) && (s>=0)){
+    var output = Math.round((35.74 + (0.6215 * t)) - (35.75 * (Math.pow(s,0.16))) + (0.4275 * (t*(Math.pow(s,0.16)))));
+  } else{
+    var output = "N/A";
+  }
+  return output;
+}
+document.getElementById("windChill").textContent = windChill(t,s);
+
+
+
+
--- lesson11/js/event.js
+++ lesson11/js/event.js
@@ -1,46 +1,64 @@
-//references the JSON file
-const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-
-// Select the page
-var townName = "";
-if (document.getElementById("preston")) {
-    townName = "Preston";
-} else if (document.getElementById("sodasprings")) {
-    townName = "Soda Springs";
-} else if (document.getElementById("fishhaven")) {
-    townName = "Fish Haven";
-}
-
-//Method to fetch the JSON file,
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
 fetch(requestURL)
+        .then(function (response) {
+           return response.json();
+        })
+        .then(function (jsonObject) {
+           console.table(jsonObject);
+     
+           const towns = jsonObject['towns'];
+           for (let i = 0; i < towns.length; i++) {
+     
+              const eventInfo = document.createElement('div');
+     
+              if (towns[i].name == "Preston" && currentURL.indexOf("preston.html") > 0 ) {
+     
+                 eventInfo.setAttribute('class', 'eventInfo');
+                 for (let x = 0; x <= towns[i].events.length; x++) {
+     
+                    let para = document.createElement('p');
+     
+                    para.textContent = towns[i].events[x];
+     
+                    eventInfo.appendChild(para);
+     
+                 }
+     
+              }
+              else if(towns[i].name == "Soda Springs" && currentURL.indexOf('sodaSprings.html') >0){
+     
+                 eventInfo.setAttribute('class', 'eventInfo');
+                 for (let x = 0; x <= towns[i].events.length; x++) {
+     
+                    let para = document.createElement('p');
+     
+                    para.textContent = towns[i].events[x];
+     
+                    eventInfo.appendChild(para);
+                 }
+              }
+              else if(towns[i].name == "Fish Haven" && currentURL.indexOf('fishHaven.html') > 0){
+     
+                 eventInfo.setAttribute('class', 'eventInfo');
+                 for (let x = 0; x <= towns[i].events.length; x++) {
+     
+                    let para = document.createElement('p');
+     
+                    para.textContent = towns[i].events[x];
+     
+                    eventInfo.appendChild(para);
+                 }
+                 
+              }
+     
+              document.getElementById('events').appendChild(eventInfo);
+           }
+     
+        });
+    
+    
 
-    .then(function (response) {
-        // return a promise 
-        return response.json();
-    })
-    //and extract JSON content
-    .then(function (jsonObject) {
-        let towns = jsonObject['towns'];
-       
-        //for loop to loop through the code and create cards
-        for (let i = 0; i < towns.length; i++) {
-            if (towns[i].name == townName) {
-                //create cards
-                let card = document.createElement('section');
-                //town name
-                let h2 = document.createElement('h2');
-                h2.textContent = towns[i].name + " Events:";
-                card.appendChild(h2);
-                document.querySelector('div.eventcard').appendChild(card);
 
-                //attempt at looping through the events
-                for (let e = 0; e < towns[i].events.length; e++) {
-                    //events
-                    let p1 = document.createElement('p');
-                    p1.textContent = towns[i].events[e];
-                    card.appendChild(p1);
-                }
 
-            }
-        }
-    });
+
+        
