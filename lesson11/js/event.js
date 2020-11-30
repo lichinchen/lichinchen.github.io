df704fbd81c03d26ce74f945c14e8225fad336f0
lichinchen <che20014@byui.edu>
2020-11-30 13:26:19 -0500

    change

--- lesson11/js/event.js
+++ lesson11/js/event.js
@@ -1,75 +1,4 @@
-/*
-const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
-fetch(requestURL)
-   .then(function (response) {
-      return response.json();
-   })
-   .then(function (jsonObject) {
-      console.table(jsonObject);
-
-      const towns = jsonObject['towns'];
-      for (let i = 0; i < towns.length; i++) {
-
-         const eventInfo = document.createElement('div');
-
-         if (towns[i].name == "Preston" && currentURL.indexOf("preston.html") > 0 ) {
-
-
-
-            eventInfo.setAttribute('class', 'eventInfo');
-            for (let x = 0; x <= towns[i].events.length; x++) {
-
-               let para = document.createElement('p');
-
-               para.textContent = towns[i].events[x];
-
-               eventInfo.appendChild(para);
-
-            }
-            
-            
-
-            
-
-
-         }
-         else if(towns[i].name == "Soda Springs" && currentURL.indexOf('sodasprings.html') >0){
-
-            eventInfo.setAttribute('class', 'eventInfo');
-            for (let x = 0; x <= towns[i].events.length; x++) {
-
-               let para = document.createElement('p');
-
-               para.textContent = towns[i].events[x];
-
-               eventInfo.appendChild(para);
-            }
-         }
-         else if(towns[i].name == "Fish Haven" && currentURL.indexOf('fishhaven.html') > 0){
-
-            eventInfo.setAttribute('class', 'eventInfo');
-            for (let x = 0; x <= towns[i].events.length; x++) {
-
-               let para = document.createElement('p');
-
-               para.textContent = towns[i].events[x];
-
-               eventInfo.appendChild(para);
-            }
-            
-         }
-
-         document.getElementById('events').appendChild(eventInfo);
-      }
-
-   });
-*/
-
-  
-
-
-
-   const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
    async function getJSON() {
        try {
@@ -81,7 +10,7 @@ fetch(requestURL)
    }
  
    async function renderGetCityEvents(cityName) {
-       let eventHTML = '<h3>Local Events</h3><ul>';
+       let eventHTML = '<h3>Upcoming Events</h3><ul>';
        let citiesJSON = await getJSON();
        let cities = citiesJSON['towns'];
        let cityData = cities.filter(city => city.name == cityName);
@@ -99,6 +28,10 @@ fetch(requestURL)
    }
 
 
+  
+
+
+   
 
 
  
