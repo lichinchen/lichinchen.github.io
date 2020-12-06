dd5c1d2f1fbf6129045c1b8aa8652ad5b794fd10
lichinchen <che20014@byui.edu>
2020-12-06 17:16:05 -0500

    change

--- lesson9/js/homepage.js
+++ lesson9/js/homepage.js
@@ -1,4 +1,3 @@
-
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 fetch(requestURL)
   .then(function (response) {
@@ -6,52 +5,63 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     const towns = jsonObject['towns'];
-    function townName(item) {
-      if (item.name == "Preston" || item.name == "Fish Haven" || item.name == "Soda Springs") {
+    function townName(town) {
+      if (town.name == "Preston" || town.name == "Fish Haven" || town.name == "Soda Springs") {
         return true;
       }
     };
+
     let assignTowns = towns.filter(townName);
+    
     for (let i = 0; i < assignTowns.length; i++) {
       let card = document.createElement('section');
-      let townDatas = document.createElement('dl');
-      townDatas.setAttribute('class', 'townDatas');
+      
       let photo = document.createElement('img');
       let name = document.createElement('h2');
       let motto = document.createElement('p');
       let year = document.createElement('div');
       let population = document.createElement('div');
       let rainfall = document.createElement('div');
+      let info = document.createElement('dl');
+          info.setAttribute('class', 'info');
       let yearNum = document.createElement('dt');
       let popNum = document.createElement('dt');
       let rainNum = document.createElement('dt');
-      let yearLabel = document.createElement('dd');
-      let popLabel = document.createElement('dd');
-      let rainLabel = document.createElement('dd');
+      let yearTag = document.createElement('dd');
+      let popTag = document.createElement('dd');
+      let rainTag = document.createElement('dd');
+
       name.textContent = assignTowns[i].name;
       motto.textContent = assignTowns[i].motto;
       yearNum.textContent = assignTowns[i].yearFounded;
       popNum.textContent = assignTowns[i].currentPopulation;
-      rainNum.textContent = assignTowns[i].averageRainfall + "\"";
-      yearLabel.textContent = "Founded";
-      popLabel.textContent = "Population";
-      rainLabel.textContent = "Rain/yr";
+      rainNum.textContent = assignTowns[i].averageRainfall;
+
+      yearTag.textContent = "Year";
+      popTag.textContent = "Population";
+      rainTag.textContent = "Rain";
       photo.setAttribute('src', "images/" + assignTowns[i].photo);
-      photo.setAttribute('alt', "The town of " + assignTowns[i].name);
+      photo.setAttribute('alt', assignTowns[i].name);
+
       card.appendChild(photo);
       card.appendChild(name);
       card.appendChild(motto);
-      card.appendChild(townDatas);
-      townDatas.appendChild(year);
-      townDatas.appendChild(population);
-      townDatas.appendChild(rainfall);
+      card.appendChild(info);
+
+      info.appendChild(year);
+      info.appendChild(population);
+      info.appendChild(rainfall);
+
       year.appendChild(yearNum);
-      year.appendChild(yearLabel);
+      year.appendChild(yearTag);
+
       population.appendChild(popNum);
-      population.appendChild(popLabel);
+      population.appendChild(popTag);
+      
       rainfall.appendChild(rainNum);
-      rainfall.appendChild(rainLabel);
-      document.querySelector('div.cards').appendChild(card);
+      rainfall.appendChild(rainTag);
+      
+      document.querySelector('div.towns').appendChild(card);
     }
   });
-
+  
