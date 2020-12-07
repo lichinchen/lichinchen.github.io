67bffff7e7a5f3d61dd4387680f57e2e039b641d
lichinchen <che20014@byui.edu>
2020-12-07 11:03:40 -0500

    change

--- lesson10/js/preston-10.js
+++ lesson10/js/preston-10.js
@@ -14,34 +14,17 @@ fetch(apiURL)
         document.getElementById('humidity').textContent = jsObject.main.humidity;
     
 });
-      
-window.onload = getWindChill();
-function getWindChill() {
-    let t = parseFloat( document.getElementById('curTemp').textContent);
-    let s = parseFloat(document.getElementById('windSpeed').textContent);
-    let windChill = document.getElementById("windChill").innerHTML = "";
+ 
 
-    if ((t <= 50) && (s > 3.0)) {
-        windChill = (35.74 + (0.6215 * t) - (35.75 * (Math.pow(s, 0.16))) + (0.4275 * t * (Math.pow(s, 0.16)))).toFixed(0);
-       let windChill = document.getElementById('windChill').innerHTML = windChill;
-    } 
-    else {
-        let windChill = document.getElementById("windChill").innerHTML = "N/A" ;
-    }
-}
 
-var t= document.getElementById("curTemp").textContent;
-var s= document.getElementById("windSpeed").textContent;
-function windChill(t,s) {
-  if((t<=60) && (s>=0)){
-    var output = Math.round((35.74 + (0.6215 * t)) - (35.75 * (Math.pow(s,0.16))) + (0.4275 * (t*(Math.pow(s,0.16)))));
-  } else{
-    var output = "N/A";
+
+window.onload = function windChill() {
+  let t = parseFloat(document.getElementById('curTemp').textContent);
+  let s= parseFloat(document.getElementById('windSpeed').textContent); 
+  let output = "N/A";
+  if (t<= 50 && s>=3){
+  let f = (35.74 + (0.6215*t)) - (35.75 * (Math.pow(s, 0.16))) + (0.4275*(t*(Math.pow(s , 0.16))));
+  output = Math.round(f);
   }
-  return output;
+  document.getElementById("windChill").innerHTML = output;
 }
-document.getElementById("windChill").textContent = windChill(t,s);
-
-
-
-
