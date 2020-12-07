fb74eee4881d851ab91100eadb1b26e311b34af9
lichinchen <che20014@byui.edu>
2020-12-06 21:15:49 -0500

    change

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -5,21 +5,26 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     const prophets = jsonObject['prophets'];
+
     for (let i = 0; i < prophets.length; i++ ) {
       let card = document.createElement('section');
       let name = document.createElement('h2');
-      let birthDate = document.createElement('p');
-      let birthPlace = document.createElement('p');
-      let portrait = document.createElement('img');
+      let bDate = document.createElement('p');
+      let bPlace = document.createElement('p');
+      let image = document.createElement('img');
+
       name.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-      birthDate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-      birthPlace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
-      portrait.setAttribute('src', prophets[i].imageurl);
-      portrait.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' ' + '-' + ' ' + prophets[i].order);
+      bDate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+      bPlace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+
+      image.setAttribute('src', prophets[i].imageurl);
+      image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' ' + '-' + ' ' + prophets[i].order);
+
       card.appendChild(name);
-      card.appendChild(birthDate);
-      card.appendChild(birthPlace);
-      card.appendChild(portrait);
+      card.appendChild(bDate);
+      card.appendChild(bPlace);
+      card.appendChild(image);
+      
       document.querySelector('div.cards').appendChild(card);
     }
   });
